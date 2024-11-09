b31cc56ed21ecfbc6a46403923f9b6febe1e459f
komaksym <kovalmaksym2@gmail.com>
2024-11-10 00:53:13 +0100

    wrote a demo for parsing data based on text, couldn't do via ROI (regions of interest) since they are not the same on all images

--- scorecard_OCR/main.py
+++ scorecard_OCR/main.py
@@ -1,21 +1,29 @@
+import pdb
 from paddleocr import PaddleOCR, draw_ocr
-from PIL import Image
+#from PIL import Image
+import pandas as pd
 
 
 ocr = PaddleOCR(lang='en') # need to run only once to download and load model into memory
-img_path = '../datasets/scorecard_images_results/maki-pitolo-charles-byrd-ufc-250-scorecard.jpg'
+img_path = 'datasets/scorecard_images_results/1-UFC 270 Ngannou vs. Gane - Scorecards - Ngannou vs. Gane.jpg'
 result = ocr.ocr(img_path, cls=False)
+
+results = {}
+
 for idx in range(len(result)):
     res = result[idx]
-    for line in res:
-        print(line)
+    for jdx in range(len(res)):
+        if res[jdx][1][0] == 'VS.':
+            results["red_fighter_name"] = res[jdx-1][1][0]
+            results["blue_fighter_name"] = res[jdx+1][1][0]
+            results["date"] = res[jdx+8][1][0]
+            results["red_fighter_total_pts_judge_one"] = res[len(res)-19][1][0]
+            results["blue_fighter_total_pts_judge_one"] = res[len(res)-17][1][0]
+            results["red_fighter_total_pts_judge_two"] = res[len(res)-16][1][0]
+            results["blue_fighter_total_pts_judge_two"] = res[len(res)-14][1][0]
+            results["red_fighter_total_pts_judge_three"] = res[len(res)-13][1][0]
+            results["blue_fighter_total_pts_judge_three"] = res[len(res)-11][1][0]
+            results["result"] = res[len(res)-1][1][0]
+        print(res[jdx])        
 
-
-result = result[0]
-image = Image.open(img_path).convert('RGB')
-boxes = [line[0] for line in result]
-txts = [line[1][0] for line in result]
-scores = [line[1][1] for line in result]
-im_show = draw_ocr(image, boxes, txts, scores, font_path='/System/Library/Fonts/Arial.ttf')
-im_show = Image.fromarray(im_show)
-im_show.save('result.jpg')
+print(results)
