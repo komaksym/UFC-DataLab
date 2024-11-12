826638f9ad96fc61d63ba902c7243b651443b3ac
komaksym <kovalmaksym2@gmail.com>
2024-11-12 23:56:56 +0100

    alpha version of the parsing script

--- scorecard_OCR/main.py
+++ scorecard_OCR/main.py
@@ -1,29 +1,79 @@
+from paddleocr import PaddleOCR
+import os 
 import pdb
-from paddleocr import PaddleOCR, draw_ocr
-#from PIL import Image
-import pandas as pd
+import re
 
 
-ocr = PaddleOCR(lang='en') # need to run only once to download and load model into memory
-img_path = 'datasets/scorecard_images_results/1-UFC 270 Ngannou vs. Gane - Scorecards - Ngannou vs. Gane.jpg'
-result = ocr.ocr(img_path, cls=False)
+# Needed data
+red_fighter_name = []
+blue_fighter_name = []
+date = []
+red_fighter_total_pts = []
+blue_fighter_total_pts = []
 
-results = {}
 
-for idx in range(len(result)):
-    res = result[idx]
-    for jdx in range(len(res)):
-        if res[jdx][1][0] == 'VS.':
-            results["red_fighter_name"] = res[jdx-1][1][0]
-            results["blue_fighter_name"] = res[jdx+1][1][0]
-            results["date"] = res[jdx+8][1][0]
-            results["red_fighter_total_pts_judge_one"] = res[len(res)-19][1][0]
-            results["blue_fighter_total_pts_judge_one"] = res[len(res)-17][1][0]
-            results["red_fighter_total_pts_judge_two"] = res[len(res)-16][1][0]
-            results["blue_fighter_total_pts_judge_two"] = res[len(res)-14][1][0]
-            results["red_fighter_total_pts_judge_three"] = res[len(res)-13][1][0]
-            results["blue_fighter_total_pts_judge_three"] = res[len(res)-11][1][0]
-            results["result"] = res[len(res)-1][1][0]
-        print(res[jdx])        
+def read_images(path):
+    """_reads images in a folder and saves paths to each of the images_
+
+    Args:
+        path (_str_): _path to a folder the images are in_
+
+    Returns:
+        _list[str]_: _paths to each of the images_
+    """
+    images = []
+
+    for image in os.listdir(path)[:20]:
+        if image.endswith('.jpg'):
+            images.append(path+image)
+    return images
+
+
+ocr = PaddleOCR(use_angle_cls=True, lang='en')
+folder_path = 'datasets/scorecard_images_results/new_version/'
+images = read_images(folder_path)
+
+for image in images:
+    result = ocr.ocr(image, cls=True)
+
+    # Parsing
+    for idx in range(len(result)):
+        print(image)
+        res = result[idx]
+        for jdx in range(len(res)):
+            # Extracting fighter names
+            if res[jdx][1][0].lower() == "vs.":
+                red_fighter_name.append(res[jdx-1][1][0])
+                blue_fighter_name.append(res[jdx+1][1][0])
+                
+            # Extracting the data of the fight
+            elif re.findall(r"(\d+\/\d+\/\d+)", res[jdx][1][0]):
+                date.append(re.findall(r"(\d+\/\d+\/\d+)", res[jdx][1][0]))
+            # Parsing the total points
+
+            elif res[jdx][1][0].lower() == "total":
+                total_points_red = res[jdx-1][1][0]
+                total_points_blue = res[jdx+1][1][0]
+                # Checking if there are total points (the fight might've ended in the first round)
+                if all(char.isdigit() for char in total_points_red):
+                    red_fighter_total_pts.append(total_points_red)
+                if all(char.isdigit() for char in total_points_blue):
+                    blue_fighter_total_pts.append(total_points_blue)
+                # If no total points
+                else:
+                    red_fighter_total_pts.append("-")
+                    blue_fighter_total_pts.append("-")
+            print(res[jdx])
+            
+results = {"red_fighter_name": red_fighter_name, "blue_fighter_name": blue_fighter_name,
+           "date": date, "red_fighter_total_pts": red_fighter_total_pts,
+           "blue_fighter_total_pts": blue_fighter_total_pts
+           }
 
 print(results)
+
+print(f"len(red_fighter_name): {len(red_fighter_name)}")
+print(f"len(blue_fighter_name): {len(blue_fighter_name)}")
+print(f"len(date): {len(date)}")
+print(f"len(red_fighter_total_pts): {len(red_fighter_total_pts)}")
+print(f"len(blue_fighter_total_pts): {len(blue_fighter_total_pts)}")
--- scorecard_OCR/testing.py
+++ scorecard_OCR/testing.py
@@ -1,10 +1,5 @@
-from paddleocr import PaddleOCR, draw_ocr
+import re
 
 
-ocr = PaddleOCR() # need to run only once to download and load model into memory
-img_path = '../datasets/scorecard_images_results/maki-pitolo-charles-byrd-ufc-250-scorecard.jpg'
-result = ocr.ocr(img_path,rec=False)
-for idx in range(len(result)):
-    res = result[idx]
-    for line in res:
-        print(line)
+foo = "Date:8/15/2020"
+print(re.findall(r"(\d+\/\d+\/\d+)", foo))
