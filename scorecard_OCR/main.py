236d7ad5e24755dff8c9eba292d1d7ae5f2266ef
komaksym <kovalmaksym2@gmail.com>
2024-11-14 22:18:20 +0100

    multiprocessing script for parsing new_version scorecards

--- scorecard_OCR/main.py
+++ scorecard_OCR/main.py
@@ -1,10 +1,12 @@
-from paddleocr import PaddleOCR
-import os 
-import pdb
 import re
+from paddleocr import PaddleOCR
+import os
+import pdb
+from tqdm import tqdm
+from multiprocessing import Pool
+import pandas as pd
 
 
-# Needed data
 red_fighter_name = []
 blue_fighter_name = []
 date = []
@@ -13,67 +15,111 @@ blue_fighter_total_pts = []
 
 
 def read_images(path):
-    """_reads images in a folder and saves paths to each of the images_
+    """Reads images in a folder and saves paths to each of the images.
 
     Args:
-        path (_str_): _path to a folder the images are in_
+        path (str): Path to a folder the images are in.
 
     Returns:
-        _list[str]_: _paths to each of the images_
+        list[str]: Paths to each of the images.
     """
-    images = []
-
-    for image in os.listdir(path)[:20]:
-        if image.endswith('.jpg'):
-            images.append(path+image)
+    images = [os.path.join(path, image) for image in os.listdir(path)[:200] if image.endswith('.jpg')]
     return images
 
 
-ocr = PaddleOCR(use_angle_cls=True, lang='en')
-folder_path = 'datasets/scorecard_images_results/new_version/'
-images = read_images(folder_path)
+def parse_image(image):
+    # Initialize PaddleOCR instance here
+    ocr_instance = PaddleOCR(use_angle_cls=False, lang='en')
 
-for image in images:
-    result = ocr.ocr(image, cls=True)
+    # Needed data
+    temp_red_fighter_name = []
+    temp_blue_fighter_name = []
+    temp_date = []
+    temp_red_fighter_total_pts = []
+    temp_blue_fighter_total_pts = []
+
+    # Ocr instance
+    result = ocr_instance.ocr(image, cls=False)
 
     # Parsing
-    for idx in range(len(result)):
+    for res in result:
         print(image)
-        res = result[idx]
-        for jdx in range(len(res)):
+        for jdx, item in enumerate(res):
+            text = item[1][0]
             # Extracting fighter names
-            if res[jdx][1][0].lower() == "vs.":
-                red_fighter_name.append(res[jdx-1][1][0])
-                blue_fighter_name.append(res[jdx+1][1][0])
-                
-            # Extracting the data of the fight
-            elif re.findall(r"(\d+\/\d+\/\d+)", res[jdx][1][0]):
-                date.append(re.findall(r"(\d+\/\d+\/\d+)", res[jdx][1][0]))
-            # Parsing the total points
+            if text.lower() == "vs.":
+                temp_red_fighter_name.append(res[jdx-1][1][0])
+                temp_blue_fighter_name.append(res[jdx+1][1][0])
 
-            elif res[jdx][1][0].lower() == "total":
+            # Extracting the date of the fight
+            elif re.findall(r"(\d+\/\d+\/\d+)", text):
+                temp_date.append(*re.findall(r"(\d+\/\d+\/\d+)", text))
+
+            # Parsing the total points
+            elif sum(1 for w, t in zip(text.lower(), "total") if w != t) < 2 and 4 <= len(text) <= 5:
                 total_points_red = res[jdx-1][1][0]
                 total_points_blue = res[jdx+1][1][0]
-                # Checking if there are total points (the fight might've ended in the first round)
-                if all(char.isdigit() for char in total_points_red):
-                    red_fighter_total_pts.append(total_points_red)
-                if all(char.isdigit() for char in total_points_blue):
-                    blue_fighter_total_pts.append(total_points_blue)
-                # If no total points
+                if total_points_red.lower() == "total" or total_points_blue.lower() == "total":
+                    temp_red_fighter_total_pts.append("-")
+                    temp_blue_fighter_total_pts.append("-")
                 else:
-                    red_fighter_total_pts.append("-")
-                    blue_fighter_total_pts.append("-")
-            print(res[jdx])
-            
-results = {"red_fighter_name": red_fighter_name, "blue_fighter_name": blue_fighter_name,
-           "date": date, "red_fighter_total_pts": red_fighter_total_pts,
-           "blue_fighter_total_pts": blue_fighter_total_pts
-           }
+                    temp_red_fighter_total_pts.append(total_points_red)
+                    temp_blue_fighter_total_pts.append(total_points_blue)
 
-print(results)
+    if len(temp_red_fighter_total_pts) != 3 or len(temp_blue_fighter_total_pts) != 3 \
+            or len(temp_date) != 1 or len(temp_red_fighter_name) != 1 \
+            or len(temp_blue_fighter_name) != 1:
+        results = {
+            "red_fighter_name": temp_red_fighter_name,
+            "blue_fighter_name": temp_blue_fighter_name,
+            "date": temp_date,
+            "red_fighter_total_pts": temp_red_fighter_total_pts,
+            "blue_fighter_total_pts": temp_blue_fighter_total_pts
+        }
+        print("\n\nOUTLIER:\n\n")
+        print(f"\n\n{item[1][0]}\n\n")
+        print(f"\n\n\n{results}\n\n\n")
+        print(f"len(red_fighter_name): {len(temp_red_fighter_name)}")
+        print(f"len(blue_fighter_name): {len(temp_blue_fighter_name)}")
+        print(f"len(date): {len(temp_date)}")
+        print(f"len(red_fighter_total_pts): {len(temp_red_fighter_total_pts)}")
+        print(f"len(blue_fighter_total_pts): {len(temp_blue_fighter_total_pts)}")
 
-print(f"len(red_fighter_name): {len(red_fighter_name)}")
-print(f"len(blue_fighter_name): {len(blue_fighter_name)}")
-print(f"len(date): {len(date)}")
-print(f"len(red_fighter_total_pts): {len(red_fighter_total_pts)}")
-print(f"len(blue_fighter_total_pts): {len(blue_fighter_total_pts)}")
+    red_fighter_name.append(temp_red_fighter_name)
+    blue_fighter_name.append(temp_blue_fighter_name)
+    date.append(temp_date)
+    red_fighter_total_pts.append(temp_red_fighter_total_pts)
+    blue_fighter_total_pts.append(temp_blue_fighter_total_pts)
+
+    return {"red_fighter_name": red_fighter_name,
+            "blue_fighter_name": blue_fighter_name,
+            "date": date,
+            "red_fighter_total_pts": red_fighter_total_pts,
+            "blue_fighter_total_pts": blue_fighter_total_pts}
+
+
+def main():
+    folder_path = 'datasets/scorecard_images_results/new_version/'
+    images = read_images(folder_path)
+
+    collected_results = []
+
+    # Multiprocessing
+    with Pool(8) as pool:
+        for result in tqdm(pool.imap(parse_image, images), total=len(images), unit="image"):
+            collected_results.append(result)
+
+    results = {"red_fighter_name": [entry["red_fighter_name"][0][0] for entry in collected_results],
+               "blue_fighter_name": [entry["blue_fighter_name"][0][0] for entry in collected_results],
+               "date": [entry["date"][0][0] for entry in collected_results],
+               "red_fighter_total_pts": [entry["red_fighter_total_pts"][0] for entry in collected_results],
+               "blue_fighter_total_pts": [entry["blue_fighter_total_pts"][0] for entry in collected_results]}
+     
+    results_df = pd.DataFrame(results)
+    print(results_df)
+
+    results_df.to_csv("datasets/parsed_scorecards.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
--- scorecard_OCR/testing.py
+++ scorecard_OCR/testing.py
@@ -1,5 +1,71 @@
 import re
+from paddleocr import PaddleOCR
+import os 
+import pdb
+import re
 
 
-foo = "Date:8/15/2020"
-print(re.findall(r"(\d+\/\d+\/\d+)", foo))
+def parse_image(image):
+
+    # Needed data
+    red_fighter_name = []
+    blue_fighter_name = []
+    date = []
+    red_fighter_total_pts = []
+    blue_fighter_total_pts = []
+
+    result = ocr.ocr(image, cls=True)
+
+    # Parsing
+    for idx in range(len(result)):
+        print(image)
+        res = result[idx]
+        for jdx in range(len(res)):
+            # Extracting fighter names
+            if res[jdx][1][0].lower() == "vs.":
+                red_fighter_name.append(res[jdx-1][1][0])
+                blue_fighter_name.append(res[jdx+1][1][0])
+                
+            # Extracting the data of the fight
+            elif re.findall(r"(\d+\/\d+\/\d+)", res[jdx][1][0]):
+                date.append(*re.findall(r"(\d+\/\d+\/\d+)", res[jdx][1][0]))
+
+            
+            # Parsing the total points
+            elif sum(1 for w, t in zip(res[jdx][1][0].lower(), "total") if w != t) < 2 and len(res[jdx][1][0]) <= 5 and len(res[jdx][1][0]) >= 4:
+                print(f"\n\n{res[jdx][1][0].lower()}\n\n")
+                total_points_red = res[jdx-1][1][0]
+                total_points_blue = res[jdx+1][1][0]
+                
+                if total_points_red.lower() == "total" or total_points_blue.lower() == "total":
+                    red_fighter_total_pts.append("-")
+                    blue_fighter_total_pts.append("-")
+
+                else:
+                    red_fighter_total_pts.append(total_points_red)
+                    blue_fighter_total_pts.append(total_points_blue)
+
+            print(res[jdx])
+
+    if len(red_fighter_total_pts) != 3 or len(blue_fighter_total_pts) != 3 or len(date) != 1 or len(red_fighter_name) != 1 or len(blue_fighter_name) != 1:
+        results = {"red_fighter_name": red_fighter_name, "blue_fighter_name": blue_fighter_name,
+                   "date": date, "red_fighter_total_pts": red_fighter_total_pts,
+                   "blue_fighter_total_pts": blue_fighter_total_pts
+                   }
+
+        # Print the results
+        print(f"\n\n\n{results}\n\n\n")
+
+        # Print lengths of data lists
+        print(f"len(red_fighter_name): {len(red_fighter_name)}")
+        print(f"len(blue_fighter_name): {len(blue_fighter_name)}")
+        print(f"len(date): {len(date)}")
+        print(f"len(red_fighter_total_pts): {len(red_fighter_total_pts)}")
+        print(f"len(blue_fighter_total_pts): {len(blue_fighter_total_pts)}")
+
+        print(hi)
+
+
+ocr = PaddleOCR(use_angle_cls=True, lang='en')
+folder_path = 'datasets/scorecard_images_results/new_version/1421.jpg'
+images = parse_image(folder_path)
