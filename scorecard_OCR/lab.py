6e02d8ffd52bfba1862c3f67ddad78da3b70eaaf
komaksym <kovalmaksym2@gmail.com>
2024-11-17 21:01:45 +0100

    did some work merging and cleaning the parsed scorecards up

--- scorecard_OCR/lab.py
+++ scorecard_OCR/lab.py
@@ -1,39 +1,12 @@
-import re
-from paddleocr import PaddleOCR
-import os
-import pdb
-from tqdm import tqdm
-from multiprocessing import Pool
-import pandas as pd
+months = {
+    "January": 1, "February": 2, "March": 3,
+    "April": 4, "May": 5, "June": 6,
+    "July": 7, "August": 8, "September": 9,
+    "October": 10, "November": 11, "December": 12
+}
+
+foo = "October 26, 2024"
 
 
-def read_images(path):
-    """Reads images in a folder and saves paths to each of the images.
-
-    Args:
-        path (str): Path to a folder the images are in.
-
-    Returns:
-        list[str]: Paths to each of the images.
-    """
-    images = [os.path.join(path, image) for image in os.listdir(path) if image.endswith('.jpg')]
-    return images[720], images[719], images[715], images[615], images[591], images[588], images[559]
-
-
-def parse_image(image):
-    # Initialize PaddleOCR instance here
-    print(image)
-
-
-def main():
-    folder_path = 'datasets/scorecards/scraped_scorecards/scorecard_images_results/new_version/'
-    images = read_images(folder_path)
-
-    # Multiprocessing
-    with Pool(8) as pool:
-        for _ in tqdm(pool.imap(parse_image, images), total=len(images), unit="image"):
-            pass
-
-
-if __name__ == "__main__":
-    main()
+month, day, year = foo.replace(",", "").split()
+print(f"{day}/{months[month]}/{year}")
