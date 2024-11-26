a921d562f0a78124edc7c700685d91f9817eaedf
komaksym <kovalmaksym2@gmail.com>
2024-11-26 01:55:33 +0100

    zipped the datasets up

---
+++ README.md
@@ -0,0 +1,48 @@
+# UFC Fights Dataset Collection
+
+This repo collects and organizes UFC Fight data, including stats, scorecards, and OCR-processed results. It provides tools for scraping, parsing, and analyzing fight information, making it useful for sports analytics, machine learning and research.
+
+## Features
+- Scraping UFC stats and scorecards.
+- OCR processing of scorecard images.
+- Organized dataset storage for analysis.
+
+## Installation
+1. Clone the repository:
+```bash
+git clone https://github.com/komaksym/UFC_fights_dataset_collection.git
+2. Install dependancies:
+pip install -r requirements.txt
+
+## Usage
+1. To scrape  UFC stats:
+cd scraping/ufc_stats_scraping
+scrapy crawl ufc_spider -O results.json
+
+2. To scrape UFC scorecards:
+cd scraping/ufc_scorecards_scraping
+scrapy crawl ufc_scorecards
+
+3. To OCR parse the scraped scorecards:
+python scorecard_OCR/app.py
+
+## Directory Structure
+.
+├- datasets			                             # Contains collected and processed data
+│       
+├── merged_stats_n_scorecards                    # Merged scorecard images and stats
+│   ├── scorecards                               # Scorecard images and OCR results
+│   │   ├── OCR_parsed_scorecards                # OCR parsed scorecards into .csv
+│   │   └── scraped_scorecard_images             # Scraped scorecards (images in .jpg format)
+│   │       ├── new_version_scorecards           # New version scorecards (.jpg)
+│   │       ├── old_version_scorecards           # Old version scorecards (.jpg)
+│   │       └── pre-new_version_scorecards       # Pre-new version scorecards (.jpg)
+│   └── stats                                    # Fight stats (.csv)
+├── scorecard_OCR                                # Script for OCR parsing the scorecards images
+└── scraping                                     # Scraping folder
+    ├── ufc_scorecards_scraping                  # Scorecard scraper
+    │   └── ufc_scorecards_scraping              # Scorecard scraper sub-folder
+    │       └── spiders                          # Scorecard spiders
+    └── ufc_stats_scraping                       # Stats scraper
+        └── ufcstats_scraping                    # Stats scraper sub-folder
+            └── spiders                          # Stats spiders
--- scraping/ufc_stats_scraping/ufcstats_scraping/spiders/stats_spider.py
+++ scraping/ufc_stats_scraping/ufcstats_scraping/spiders/stats_spider.py
@@ -4,32 +4,13 @@ from ..items import FightData
 
 
 class UFCSpider(scrapy.Spider):
-    name = "ufc_stats"
+    name = "ufc_spider"
     allowed_domains = ["ufcstats.com"]
     start_urls = ["http://ufcstats.com/statistics/events/completed?page=all"]
 
-    # Add constants for commonly used XPath bases
-    GENERAL_FIGHT_BASE_PATH = "/html/body/section/div/div/div"
-    DETAILED_TOTALS_BASE_PATH = "/html/body/section/div/div/section[2]/table/tbody/tr/td"
-    SIGSTR_TARGET_BASE_PATH = "/html/body/section/div/div/table/tbody/tr/td"
-    SIGSTR_POS_BASE_PATH = "/html/body/section/div/div/section[6]/div/div/div[1]/div"
-
-    def _safe_extract(self, response, xpath: str, default: str = "-") -> str:
-        """Helper method to safely extract XPath data with a default value"""
-        return response.xpath(xpath).get(default)
-
-    def _extract_fighter_data(self, response, base_path: str, side: str) -> dict:
-        """Helper method to extract fighter-specific data"""
-        idx = "1" if side == "red" else "2"
-        return {
-            f"{side}_fighter_name": self._safe_extract(f"{base_path}[1]/div[{idx}]/div/h3/a/text()"),
-            f"{side}_fighter_nickname": self._safe_extract(f"{base_path}[1]/div[{idx}]/div/p/text()"),
-            f"{side}_fighter_result": self._safe_extract(f"{base_path}[1]/div[{idx}]/i/text()")
-        }
-
     def parse(self, response):
         """Events and fights page"""
-        
+
         # Extract links to all UFC events
         events_links = response.xpath("//a[@class='b-link b-link_style_black']/@href").getall()
         # Go through each event
@@ -53,75 +34,79 @@ class UFCSpider(scrapy.Spider):
     def parse_fight(self, response):
         """Parse each individual fight matchup"""
         event_data = response.meta["event_data"]
+
+        # General fight data
         fight_data_item = FightData()
-
-        # Extract basic fighter data
-        red_fighter_data = self._extract_fighter_data(response, self.GENERAL_FIGHT_BASE_PATH, "red")
-        blue_fighter_data = self._extract_fighter_data(response, self.GENERAL_FIGHT_BASE_PATH, "blue")
-        fight_data_item.update(red_fighter_data)
-        fight_data_item.update(blue_fighter_data)
-
-        # Extract fight details
-        fight_data_item.update({
-            "method": self._safe_extract(f"{self.GENERAL_FIGHT_BASE_PATH}[2]/div[2]/p[1]/i[1]/i[2]/text()"),
-            "round": self._safe_extract(f"{self.GENERAL_FIGHT_BASE_PATH}[2]/div[2]/p[1]/i[2]/text()[2]"),
-            "time": self._safe_extract(f"{self.GENERAL_FIGHT_BASE_PATH}[2]/div[2]/p[1]/i[3]/text()[2]"),
-            "time_format": self._safe_extract(f"{self.GENERAL_FIGHT_BASE_PATH}[2]/div[2]/p[1]/i[4]/text()[2]"),
-            "referee": self._safe_extract(f"{self.GENERAL_FIGHT_BASE_PATH}[2]/div[2]/p[1]/i[5]/span/text()"),
-            "details": response.xpath("//p[@class='b-fight-details__text'][2]//text()[normalize-space() and not (contains(., 'Details:'))]").getall(),
-            "bout_type": self._safe_extract(f"{self.GENERAL_FIGHT_BASE_PATH}[2]/div[1]/i/text()[-1]"),
-            "bonus": self._safe_extract(f"{self.GENERAL_FIGHT_BASE_PATH}[2]/div[1]/i/img/@src")
-        })
-
-        # Add event data
-        fight_data_item.update(event_data)
-
+        general_fight_base_path = "/html/body/section/div/div/div"
+        fight_data_item["red_fighter_name"] = response.xpath(f"{general_fight_base_path}[1]/div[1]/div/h3/a/text()").get("-")
+        fight_data_item["blue_fighter_name"] = response.xpath(f"{general_fight_base_path}[1]/div[2]/div/h3/a/text()").get("-")
+        fight_data_item["red_fighter_nickname"] = response.xpath(f"{general_fight_base_path}[1]/div[1]/div/p/text()").get("-")
+        fight_data_item["blue_fighter_nickname"] = response.xpath(f"{general_fight_base_path}[1]/div[2]/div/p/text()").get("-")
+        fight_data_item["red_fighter_result"] = response.xpath(f"{general_fight_base_path}[1]/div[1]/i/text()").get("-")
+        fight_data_item["blue_fighter_result"] = response.xpath(f"{general_fight_base_path}[1]/div[2]/i/text()").get("-")
+        fight_data_item["method"] = response.xpath(f"{general_fight_base_path}[2]/div[2]/p[1]/i[1]/i[2]/text()").get("-")
+        fight_data_item["round"] = response.xpath(f"{general_fight_base_path}[2]/div[2]/p[1]/i[2]/text()[2]").get("-")
+        fight_data_item["time"] = response.xpath(f"{general_fight_base_path}[2]/div[2]/p[1]/i[3]/text()[2]").get("-")
+        fight_data_item["time_format"] = response.xpath(f"{general_fight_base_path}[2]/div[2]/p[1]/i[4]/text()[2]").get("-")
+        fight_data_item["referee"] = response.xpath(f"{general_fight_base_path}[2]/div[2]/p[1]/i[5]/span/text()").get("-")
+        fight_data_item["details"] = response.xpath("//p[@class='b-fight-details__text'][2]//text()[normalize-space() and not (contains(., 'Details:'))]").getall()
+        fight_data_item["bout_type"] = response.xpath(f"{general_fight_base_path}[2]/div[1]/i/text()").getall()[-1]
+        fight_data_item["bonus"] = response.xpath(f"{general_fight_base_path}[2]/div[1]/i/img/@src").get("-")
+        
+        # Event data
+        fight_data_item['event_name'] = event_data["event"]
+        fight_data_item['event_date'] = event_data["date"]
+        fight_data_item['event_location'] = event_data["location"]
+        
         # Detailed Fight data totals
-        fight_data_item["red_fighter_KD"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[2]/p[1]/text()")
-        fight_data_item["blue_fighter_KD"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[2]/p[2]/text()")
-        fight_data_item["red_fighter_sig_str"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[3]/p[1]/text()")
-        fight_data_item["blue_fighter_sig_str"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[3]/p[2]/text()")
-        fight_data_item["red_fighter_sig_str_pct"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[4]/p[1]/text()")
-        fight_data_item["blue_fighter_sig_str_pct"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[4]/p[2]/text()")
-        fight_data_item["red_fighter_total_str"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[5]/p[1]/text()")
-        fight_data_item["blue_fighter_total_str"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[5]/p[2]/text()")
-        fight_data_item["red_fighter_TD"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[6]/p[1]/text()")
-        fight_data_item["blue_fighter_TD"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[6]/p[2]/text()")
-        fight_data_item["red_fighter_TD_pct"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[7]/p[1]/text()")
-        fight_data_item["blue_fighter_TD_pct"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[7]/p[2]/text()")
-        fight_data_item["red_fighter_sub_att"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[8]/p[1]/text()")
-        fight_data_item["blue_fighter_sub_att"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[8]/p[2]/text()")
-        fight_data_item["red_fighter_rev"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[9]/p[1]/text()")
-        fight_data_item["blue_fighter_rev"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[9]/p[2]/text()")
-        fight_data_item["red_fighter_ctrl"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[10]/p[1]/text()")
-        fight_data_item["blue_fighter_ctrl"] = self._safe_extract(f"{self.DETAILED_TOTALS_BASE_PATH}[10]/p[2]/text()")
+        detailed_fight_totals_base_path = "/html/body/section/div/div/section[2]/table/tbody/tr/td"
+        fight_data_item["red_fighter_KD"] = response.xpath(f"{detailed_fight_totals_base_path}[2]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_KD"] = response.xpath(f"{detailed_fight_totals_base_path}[2]/p[2]/text()").get("-")
+        fight_data_item["red_fighter_sig_str"] = response.xpath(f"{detailed_fight_totals_base_path}[3]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_sig_str"] = response.xpath(f"{detailed_fight_totals_base_path}[3]/p[2]/text()").get("-")
+        fight_data_item["red_fighter_sig_str_pct"] = response.xpath(f"{detailed_fight_totals_base_path}[4]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_sig_str_pct"] = response.xpath(f"{detailed_fight_totals_base_path}[4]/p[2]/text()").get("-")
+        fight_data_item["red_fighter_total_str"] = response.xpath(f"{detailed_fight_totals_base_path}[5]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_total_str"] = response.xpath(f"{detailed_fight_totals_base_path}[5]/p[2]/text()").get("-")
+        fight_data_item["red_fighter_TD"] = response.xpath(f"{detailed_fight_totals_base_path}[6]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_TD"] = response.xpath(f"{detailed_fight_totals_base_path}[6]/p[2]/text()").get("-")
+        fight_data_item["red_fighter_TD_pct"] = response.xpath(f"{detailed_fight_totals_base_path}[7]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_TD_pct"] = response.xpath(f"{detailed_fight_totals_base_path}[7]/p[2]/text()").get("-")
+        fight_data_item["red_fighter_sub_att"] = response.xpath(f"{detailed_fight_totals_base_path}[8]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_sub_att"] = response.xpath(f"{detailed_fight_totals_base_path}[8]/p[2]/text()").get("-")
+        fight_data_item["red_fighter_rev"] = response.xpath(f"{detailed_fight_totals_base_path}[9]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_rev"] = response.xpath(f"{detailed_fight_totals_base_path}[9]/p[2]/text()").get("-")
+        fight_data_item["red_fighter_ctrl"] = response.xpath(f"{detailed_fight_totals_base_path}[10]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_ctrl"] = response.xpath(f"{detailed_fight_totals_base_path}[10]/p[2]/text()").get("-")
         
         # Detailed Fight data significant strikes
-        fight_data_item["red_fighter_sig_str_head"] = self._safe_extract(f"{self.SIGSTR_TARGET_BASE_PATH}[4]/p[1]/text()")
-        fight_data_item["blue_fighter_sig_str_head"] = self._safe_extract(f"{self.SIGSTR_TARGET_BASE_PATH}[4]/p[2]/text()")
-        fight_data_item["red_fighter_sig_str_body"] = self._safe_extract(f"{self.SIGSTR_TARGET_BASE_PATH}[5]/p[1]/text()")
-        fight_data_item["blue_fighter_sig_str_body"] = self._safe_extract(f"{self.SIGSTR_TARGET_BASE_PATH}[5]/p[2]/text()")
-        fight_data_item["red_fighter_sig_str_leg"] = self._safe_extract(f"{self.SIGSTR_TARGET_BASE_PATH}[6]/p[1]/text()")
-        fight_data_item["blue_fighter_sig_str_leg"] = self._safe_extract(f"{self.SIGSTR_TARGET_BASE_PATH}[6]/p[2]/text()")
-        fight_data_item["red_fighter_sig_str_distance"] = self._safe_extract(f"{self.SIGSTR_TARGET_BASE_PATH}[7]/p[1]/text()")
-        fight_data_item["blue_fighter_sig_str_distance"] = self._safe_extract(f"{self.SIGSTR_TARGET_BASE_PATH}[7]/p[2]/text()")
-        fight_data_item["red_fighter_sig_str_clinch"] = self._safe_extract(f"{self.SIGSTR_TARGET_BASE_PATH}[8]/p[1]/text()")
-        fight_data_item["blue_fighter_sig_str_clinch"] = self._safe_extract(f"{self.SIGSTR_TARGET_BASE_PATH}[8]/p[2]/text()")
-        fight_data_item["red_fighter_sig_str_ground"] = self._safe_extract(f"{self.SIGSTR_TARGET_BASE_PATH}[9]/p[1]/text()")
-        fight_data_item["blue_fighter_sig_str_ground"] = self._safe_extract(f"{self.SIGSTR_TARGET_BASE_PATH}[9]/p[2]/text()")
+        detailed_fight_sigstr_tar_base_path = "/html/body/section/div/div/table/tbody/tr/td"
+        detailed_fight_sigstr_pos_base_path = "/html/body/section/div/div/section[6]/div/div/div[1]/div"
+        fight_data_item["red_fighter_sig_str_head"] = response.xpath(f"{detailed_fight_sigstr_tar_base_path}[4]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_sig_str_head"] = response.xpath(f"{detailed_fight_sigstr_tar_base_path}[4]/p[2]/text()").get("-")
+        fight_data_item["red_fighter_sig_str_body"] = response.xpath(f"{detailed_fight_sigstr_tar_base_path}[5]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_sig_str_body"] = response.xpath(f"{detailed_fight_sigstr_tar_base_path}[5]/p[2]/text()").get("-")
+        fight_data_item["red_fighter_sig_str_leg"] = response.xpath(f"{detailed_fight_sigstr_tar_base_path}[6]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_sig_str_leg"] = response.xpath(f"{detailed_fight_sigstr_tar_base_path}[6]/p[2]/text()").get("-")
+        fight_data_item["red_fighter_sig_str_distance"] = response.xpath(f"{detailed_fight_sigstr_tar_base_path}[7]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_sig_str_distance"] = response.xpath(f"{detailed_fight_sigstr_tar_base_path}[7]/p[2]/text()").get("-")
+        fight_data_item["red_fighter_sig_str_clinch"] = response.xpath(f"{detailed_fight_sigstr_tar_base_path}[8]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_sig_str_clinch"] = response.xpath(f"{detailed_fight_sigstr_tar_base_path}[8]/p[2]/text()").get("-")
+        fight_data_item["red_fighter_sig_str_ground"] = response.xpath(f"{detailed_fight_sigstr_tar_base_path}[9]/p[1]/text()").get("-")
+        fight_data_item["blue_fighter_sig_str_ground"] = response.xpath(f"{detailed_fight_sigstr_tar_base_path}[9]/p[2]/text()").get("-")
         # Detailed Fight pct data significant strikes by target
-        fight_data_item["red_fighter_sig_str_head_pct"] = self._safe_extract(f"{self.SIGSTR_POS_BASE_PATH}[1]/div/div[2]/div[1]/i[1]/text()")
-        fight_data_item["blue_fighter_sig_str_head_pct"] = self._safe_extract(f"{self.SIGSTR_POS_BASE_PATH}[1]/div/div[2]/div[1]/i[3]/text()")
-        fight_data_item["red_fighter_sig_str_body_pct"] = self._safe_extract(f"{self.SIGSTR_POS_BASE_PATH}[1]/div/div[2]/div[2]/i[1]/text()")
-        fight_data_item["blue_fighter_sig_str_body_pct"] = self._safe_extract(f"{self.SIGSTR_POS_BASE_PATH}[1]/div/div[2]/div[2]/i[3]/text()")
-        fight_data_item["red_fighter_sig_str_leg_pct"] = self._safe_extract(f"{self.SIGSTR_POS_BASE_PATH}[1]/div/div[2]/div[3]/i[1]/text()")
-        fight_data_item["blue_fighter_sig_str_leg_pct"] = self._safe_extract(f"{self.SIGSTR_POS_BASE_PATH}[1]/div/div[2]/div[3]/i[3]/text()")
+        fight_data_item["red_fighter_sig_str_head_pct"] = response.xpath(f"{detailed_fight_sigstr_pos_base_path}[1]/div/div[2]/div[1]/i[1]/text()").get("-")
+        fight_data_item["blue_fighter_sig_str_head_pct"] = response.xpath(f"{detailed_fight_sigstr_pos_base_path}[1]/div/div[2]/div[1]/i[3]/text()").get("-")
+        fight_data_item["red_fighter_sig_str_body_pct"] = response.xpath(f"{detailed_fight_sigstr_pos_base_path}[1]/div/div[2]/div[2]/i[1]/text()").get("-")
+        fight_data_item["blue_fighter_sig_str_body_pct"] = response.xpath(f"{detailed_fight_sigstr_pos_base_path}[1]/div/div[2]/div[2]/i[3]/text()").get("-")
+        fight_data_item["red_fighter_sig_str_leg_pct"] = response.xpath(f"{detailed_fight_sigstr_pos_base_path}[1]/div/div[2]/div[3]/i[1]/text()").get("-")
+        fight_data_item["blue_fighter_sig_str_leg_pct"] = response.xpath(f"{detailed_fight_sigstr_pos_base_path}[1]/div/div[2]/div[3]/i[3]/text()").get("-")
         # Detailed Fight pct data significant strikes by position
-        fight_data_item["red_fighter_sig_str_distance_pct"] = self._safe_extract(f"{self.SIGSTR_POS_BASE_PATH}[2]/div/div[2]/div[1]/i[1]/text()")
-        fight_data_item["blue_fighter_sig_str_distance_pct"] = self._safe_extract(f"{self.SIGSTR_POS_BASE_PATH}[2]/div/div[2]/div[1]/i[3]/text()")
-        fight_data_item["red_fighter_sig_str_clinch_pct"] = self._safe_extract(f"{self.SIGSTR_POS_BASE_PATH}[2]/div/div[2]/div[2]/i[1]/text()")
-        fight_data_item["blue_fighter_sig_str_clinch_pct"] = self._safe_extract(f"{self.SIGSTR_POS_BASE_PATH}[2]/div/div[2]/div[2]/i[3]/text()")
-        fight_data_item["red_fighter_sig_str_ground_pct"] = self._safe_extract(f"{self.SIGSTR_POS_BASE_PATH}[2]/div/div[2]/div[3]/i[1]/text()")
-        fight_data_item["blue_fighter_sig_str_ground_pct"] = self._safe_extract(f"{self.SIGSTR_POS_BASE_PATH}[2]/div/div[2]/div[3]/i[3]/text()")
+        fight_data_item["red_fighter_sig_str_distance_pct"] = response.xpath(f"{detailed_fight_sigstr_pos_base_path}[2]/div/div[2]/div[1]/i[1]/text()").get("-")
+        fight_data_item["blue_fighter_sig_str_distance_pct"] = response.xpath(f"{detailed_fight_sigstr_pos_base_path}[2]/div/div[2]/div[1]/i[3]/text()").get("-")
+        fight_data_item["red_fighter_sig_str_clinch_pct"] = response.xpath(f"{detailed_fight_sigstr_pos_base_path}[2]/div/div[2]/div[2]/i[1]/text()").get("-")
+        fight_data_item["blue_fighter_sig_str_clinch_pct"] = response.xpath(f"{detailed_fight_sigstr_pos_base_path}[2]/div/div[2]/div[2]/i[3]/text()").get("-")
+        fight_data_item["red_fighter_sig_str_ground_pct"] = response.xpath(f"{detailed_fight_sigstr_pos_base_path}[2]/div/div[2]/div[3]/i[1]/text()").get("-")
+        fight_data_item["blue_fighter_sig_str_ground_pct"] = response.xpath(f"{detailed_fight_sigstr_pos_base_path}[2]/div/div[2]/div[3]/i[3]/text()").get("-")
 
         yield fight_data_item 
