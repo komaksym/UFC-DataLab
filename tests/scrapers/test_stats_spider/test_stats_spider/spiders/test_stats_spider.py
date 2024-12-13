4e5d68f91b1bd07906e9b91dd4ffc9cf63beda8e
komaksym <kovalmaksym2@gmail.com>
2024-12-13 02:02:29 +0100

    set up and provided a correct path to the local pages for testers to scrape

--- tests/scrapers/test_stats_spider/test_stats_spider/spiders/test_stats_spider.py
+++ tests/scrapers/test_stats_spider/test_stats_spider/spiders/test_stats_spider.py
@@ -1,31 +1,14 @@
-"""import scrapy
+import scrapy
 import pytest
+from pathlib import Path
+
+
 
 
 class Test_Stats_Spider(scrapy.Spider):
-    name = "test_stats_spider"
-    start_urls = ["file:///Users/koval/dev/UFC_DataLab/tests/scrapers/stats/dummy_events_page/events_page.html"]
+    name = "tester"
+    file_path = Path(__file__).parent / "dummy_pages_to_scrape/dummy_events_page/events_page.html"
+    start_urls = [f"file://{file_path.resolve()}"]
 
-    def parse(self, response):"""
+    #def parse(self, response):
 
-from pathlib import Path
-
-import scrapy
-
-
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-
-    def start_requests(self):
-        urls = [
-            "https://quotes.toscrape.com/page/1/",
-            "https://quotes.toscrape.com/page/2/",
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
